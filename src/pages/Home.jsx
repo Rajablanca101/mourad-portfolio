85102efd25e7c16f12d8e8beff5a1eead0051eeb
Mourad <167190354+Rajablanca101@users.noreply.github.com>
2025-05-12 14:59:12 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = ({ onSelectItem, items }) => {
 
       <section className="banner-section">
         <img
-          src="../images/Home/Typography.png"
+          src="https://github.com/Rajablanca101/mourad-portfolio/blob/main/public/images/Home/Typography.png"
           alt="Typography skills"
           className="full-width-image"
         />
